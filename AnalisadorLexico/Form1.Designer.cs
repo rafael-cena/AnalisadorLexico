6dea58c256d0e9936c64474d07fd1ded3ebcb5a0
rafael-cena <rafaelcena4@gmail.com>
2026-02-25 18:24:28 -0300

    incialização do projeto. Caixa de texto e indexação das linhas

---
+++ AnalisadorLexico/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace AnalisadorLexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbEditor = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnNumbers = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // rtbEditor
+            // 
+            this.rtbEditor.AcceptsTab = true;
+            this.rtbEditor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbEditor.BackColor = System.Drawing.SystemColors.ControlText;
+            this.rtbEditor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rtbEditor.ForeColor = System.Drawing.SystemColors.Control;
+            this.rtbEditor.Location = new System.Drawing.Point(31, 35);
+            this.rtbEditor.Name = "rtbEditor";
+            this.rtbEditor.Size = new System.Drawing.Size(769, 412);
+            this.rtbEditor.TabIndex = 0;
+            this.rtbEditor.Text = "";
+            this.rtbEditor.ZoomFactor = 1.2F;
+            this.rtbEditor.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 35);
+            this.panel2.TabIndex = 2;
+            // 
+            // pnNumbers
+            // 
+            this.pnNumbers.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pnNumbers.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnNumbers.Location = new System.Drawing.Point(0, 35);
+            this.pnNumbers.Name = "pnNumbers";
+            this.pnNumbers.Size = new System.Drawing.Size(35, 415);
+            this.pnNumbers.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnNumbers);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.rtbEditor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbEditor;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnNumbers;
+    }
+}
+
---
+++ AnalisadorLexico/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalisadorLexico
+{
+    public partial class Form1 : Form
+    {
+        int index = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Define o painel de números para desenhar
+            pnNumbers.Paint += PnNumbers_Paint;
+
+            // Força redesenho ao digitar
+            rtbEditor.TextChanged += (s, e) => pnNumbers.Invalidate();
+
+            // Força redesenho ao rolar (scroll)
+            rtbEditor.VScroll += (s, e) => pnNumbers.Invalidate();
+
+            // Garante que o painel de números seja transparente ou da cor certa
+            pnNumbers.BackColor = Color.LightGray;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void PnNumbers_Paint(object sender, PaintEventArgs e)
+        {
+            // Calcula o número de linhas e desenha-as
+            int firstVisibleLine = rtbEditor.GetLineFromCharIndex(rtbEditor.GetCharIndexFromPosition(new Point(0, 0)));
+            int lastVisibleLine = rtbEditor.GetLineFromCharIndex(rtbEditor.GetCharIndexFromPosition(new Point(0, rtbEditor.Height)));
+
+            for (int i = firstVisibleLine; i <= lastVisibleLine; i++)
+            {
+                Point pos = rtbEditor.GetPositionFromCharIndex(rtbEditor.GetFirstCharIndexFromLine(i));
+
+                // Desenha o número da linha ao lado
+                e.Graphics.DrawString(
+                    (i + 1).ToString(),
+                    rtbEditor.Font,
+                    Brushes.Black,
+                    pnNumbers.Width - 25, // Alinhamento à direita do painel
+                    pos.Y
+                );
+            }
+        }
+    }
+}
