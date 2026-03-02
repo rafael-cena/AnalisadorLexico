df87909656a63772d255e3ec7e0ce0a3121c1064
LeonardoPlins <leonardoplins181@gmail.com>
2026-03-02 13:26:44 -0300

    Adicionando Salvar como, limpar, carregar, executar

--- AnalisadorLexico/Form1.Designer.cs
+++ AnalisadorLexico/Form1.Designer.cs
@@ -31,6 +31,18 @@
             this.rtbEditor = new System.Windows.Forms.RichTextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pnNumbers = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arqMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.btSalvar = new System.Windows.Forms.ToolStripMenuItem();
+            this.btSalvarComo = new System.Windows.Forms.ToolStripMenuItem();
+            this.btCarregar = new System.Windows.Forms.ToolStripMenuItem();
+            this.btLimpar = new System.Windows.Forms.ToolStripMenuItem();
+            this.btExecutar = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // rtbEditor
@@ -42,9 +54,10 @@
             this.rtbEditor.BackColor = System.Drawing.SystemColors.ControlText;
             this.rtbEditor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.rtbEditor.ForeColor = System.Drawing.SystemColors.Control;
-            this.rtbEditor.Location = new System.Drawing.Point(31, 35);
+            this.rtbEditor.Location = new System.Drawing.Point(23, 28);
+            this.rtbEditor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rtbEditor.Name = "rtbEditor";
-            this.rtbEditor.Size = new System.Drawing.Size(769, 412);
+            this.rtbEditor.Size = new System.Drawing.Size(578, 336);
             this.rtbEditor.TabIndex = 0;
             this.rtbEditor.Text = "";
             this.rtbEditor.ZoomFactor = 1.2F;
@@ -53,32 +66,113 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.Controls.Add(this.labelNome);
+            this.panel2.Controls.Add(this.menuStrip1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 35);
+            this.panel2.Size = new System.Drawing.Size(600, 28);
             this.panel2.TabIndex = 2;
             // 
             // pnNumbers
             // 
             this.pnNumbers.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.pnNumbers.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnNumbers.Location = new System.Drawing.Point(0, 35);
+            this.pnNumbers.Location = new System.Drawing.Point(0, 28);
+            this.pnNumbers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnNumbers.Name = "pnNumbers";
-            this.pnNumbers.Size = new System.Drawing.Size(35, 415);
+            this.pnNumbers.Size = new System.Drawing.Size(26, 338);
             this.pnNumbers.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arqMenu,
+            this.btExecutar});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arqMenu
+            // 
+            this.arqMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btSalvar,
+            this.btSalvarComo,
+            this.btCarregar,
+            this.btLimpar});
+            this.arqMenu.Name = "arqMenu";
+            this.arqMenu.Size = new System.Drawing.Size(61, 20);
+            this.arqMenu.Text = "Arquivo";
+            // 
+            // btSalvar
+            // 
+            this.btSalvar.Name = "btSalvar";
+            this.btSalvar.Size = new System.Drawing.Size(180, 22);
+            this.btSalvar.Text = "Salvar";
+            this.btSalvar.Click += new System.EventHandler(this.btSalvar_Click);
+            // 
+            // btSalvarComo
+            // 
+            this.btSalvarComo.Name = "btSalvarComo";
+            this.btSalvarComo.Size = new System.Drawing.Size(180, 22);
+            this.btSalvarComo.Text = "Salvar Como";
+            this.btSalvarComo.Click += new System.EventHandler(this.btSalvarComo_Click);
+            // 
+            // btCarregar
+            // 
+            this.btCarregar.Name = "btCarregar";
+            this.btCarregar.Size = new System.Drawing.Size(180, 22);
+            this.btCarregar.Text = "Carregar";
+            this.btCarregar.Click += new System.EventHandler(this.btCarregar_Click);
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(180, 22);
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // btExecutar
+            // 
+            this.btExecutar.Name = "btExecutar";
+            this.btExecutar.Size = new System.Drawing.Size(64, 20);
+            this.btExecutar.Text = "Executar";
+            this.btExecutar.Click += new System.EventHandler(this.btExecutar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.labelNome.Location = new System.Drawing.Point(508, 5);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(59, 13);
+            this.labelNome.TabIndex = 1;
+            this.labelNome.Text = "Sem Nome";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Desktop;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.pnNumbers);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.rtbEditor);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -88,6 +182,16 @@
         private System.Windows.Forms.RichTextBox rtbEditor;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel pnNumbers;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arqMenu;
+        private System.Windows.Forms.ToolStripMenuItem btSalvar;
+        private System.Windows.Forms.ToolStripMenuItem btSalvarComo;
+        private System.Windows.Forms.ToolStripMenuItem btCarregar;
+        private System.Windows.Forms.ToolStripMenuItem btLimpar;
+        private System.Windows.Forms.ToolStripMenuItem btExecutar;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label labelNome;
     }
 }
 
--- AnalisadorLexico/Form1.cs
+++ AnalisadorLexico/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,6 +55,104 @@ namespace AnalisadorLexico
                     pos.Y
                 );
             }
+        }
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btSalvarComo_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            // Configure dialog properties
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Title = "Save Text File";
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.FileName = labelNome.Text;
+            saveFileDialog1.RestoreDirectory = true;
+            saveFileDialog1.DefaultExt = "txt";
+
+            // Show dialog and check result
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Save file using the selected path
+                File.WriteAllText(saveFileDialog1.FileName, rtbEditor.Text);
+            }
+        }
+
+        private void btCarregar_Click(object sender, EventArgs e)
+        {
+            string selectedFilePath = "";
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePath = openFileDialog1.FileName;
+                string content = File.ReadAllText(selectedFilePath);
+                rtbEditor.Text = content;
+            }
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            rtbEditor.Clear();
+        }
+
+        private void btExecutar_Click(object sender, EventArgs e)
+        {
+            verificaBlocoPrincipal();
+        }
+
+        private bool verificaBlocoPrincipal()
+        {
+            string codigo = rtbEditor.Text;
+            string palavraLida = "";
+            int pos = 0;
+            while (pos < codigo.Length && codigo[pos] != ' ')
+            {
+                palavraLida += codigo[pos];
+                pos++;
+            }
+            if (palavraLida != "BLOCO")
+            {
+                return false;
+            }
+            pos++;
+            palavraLida = "";
+            while (pos < codigo.Length && codigo[pos] != '.')
+            {
+                palavraLida += codigo[pos];
+                pos++;
+            }
+            labelNome.Text = palavraLida;
+            pos++;
+            palavraLida = "";
+            while (pos < codigo.Length && codigo[pos] != '{')
+            {
+                palavraLida += codigo[pos];
+                pos++;
+            }
+            if (pos >= codigo.Length)
+            {
+                return false;
+            }
+            pos++;
+            while (pos < codigo.Length && codigo[pos] != '}')
+            {
+                palavraLida += codigo[pos];
+                pos++;
+            }
+            if (pos >= codigo.Length)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
